0fada7484395dd153ab26fe429b5d21363e2c70e
priestkupa <samsungmaraire@gmail.com>
2024-12-03 04:52:51 +0200

    Update config.js

--- config.js
+++ config.js
@@ -8,9 +8,9 @@ module.exports = {
 SESSION_ID: process.env.SESSION_ID || "TuRBWKqZ#u4Oh320ATBX6auvW1GiZ_qEY7wqra1TNBKZDDOqhaZM", //paste your session_id
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true", 
 MODE: process.env.MODE || "public",
-AUTO_VOICE: process.env.AUTO_VOICE || "true",
+AUTO_VOICE: process.env.AUTO_VOICE || "false",
 AUTO_STICKER: process.env.AUTO_STICKER || "false",
 AUTO_REPLY: process.env.AUTO_REPLY || "false",
 AUTO_REACT: process.env.AUTO_REACT || "false",
-FAKE_RECORDING: process.env.FAKE_RECORDING || "true",    
+FAKE_RECORDING: process.env.FAKE_RECORDING || "false",    
 };
